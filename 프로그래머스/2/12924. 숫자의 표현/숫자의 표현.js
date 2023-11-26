19cf1f2029d10a0b0034922cbb731f612f1e4936
YukDDong <112530541+YukDDong@users.noreply.github.com>
2023-11-26 20:09:01 +0900

    [level 2] Title: 숫자의 표현, Time: 1.47 ms, Memory: 36.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
@@ -0,0 +1,15 @@
+function solution(n) {
+    let answer = 1;
+    for(let i=1; i<(n/2); i++){
+        let sum = 0;
+        let j = i;
+        while(sum<n){
+            sum=sum+j
+            j++
+        }
+        if(sum===n){
+            answer++
+        }
+    }
+    return answer;
+}
